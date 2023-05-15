77993e518414c3b2e667d613254d0f7abfbb6fe3
Pratik Das <pratikd2000@gmail.com>
2023-05-15 07:37:35 +0530

    added test

--- app-rat-web/src/App.test.js
+++ app-rat-web/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app details form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Select Application Type/i);
+  expect(textElement).toBeInTheDocument();
 });
